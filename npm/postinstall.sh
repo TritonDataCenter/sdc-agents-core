dc20280bde987de93e077875d772cf24ee1a2fad
Pedro P. Candel <kusorbox@gmail.com>
2015-04-21 10:57:20 +0200

    AGENT-882: Ensure agent instance uuid is created the first time we setup in HN

--- npm/postinstall.sh
+++ npm/postinstall.sh
@@ -110,6 +110,16 @@ function adopt_instance()
     echo "Adopted service ${AGENT} to instance ${instance_uuid}"
 }
 
+function save_instance_uuid()
+{
+    local instance_uuid=$(cat $ETC_DIR/$AGENT)
+
+    if [[ -z ${instance_uuid} ]]; then
+        instance_uuid=$(uuid -v4)
+        echo $instance_uuid > $ETC_DIR/$AGENT
+    fi
+}
+
 # ---- mainline
 
 # If agentsshar is being installed on an old SAPI/CN, we can leave this instance
@@ -138,9 +148,10 @@ if [[ $? == 0 ]]; then
 fi
 
 # case 1) is first time this agent is installed on the headnode. We just need
-# to add the instance to config-agent since headnode.sh takes care of adopting
-# it into SAPI
+# to save the instance uuid since headnode.sh takes care of adopting it into
+# SAPI
 if [[ $is_headnode == "true" ]] && [[ $have_sapi == "false" ]]; then
+    save_instance_uuid
     exit 0
 fi
 
